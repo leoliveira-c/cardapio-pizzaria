fe64f839cef397a0bb6ebaf43529a8cec142dae6
leoliveira-c <leo.oliveira2015@outlook.com>
2023-02-13 22:19:25 -0300

    commit full project

---
+++ download-package/Cardapio Pizzaria/js/models.js
@@ -0,0 +1,10 @@
+let modelsJson = [
+    {id:1, name:'Pastel de Calabresa ', img:'img/CALABRESA.jpg', price:[7.00, 11.00, 15.00], sizes:['1/72', '1/48', '1/32'], description:'Pastel com recheio de Calabresa'},
+    {id:2, name:'Pastel de Camarão', img:'img/CAMARAO.jpg', price:[9.00, 12.00, 16.00], sizes:['1/72', '1/48', '1/32'], description:'Pastel com recheio de Camarão'},
+    {id:3, name:'Pastel de Chocolate', img:'img/CHOCOLATE.jpg', price:[8.00, 11.00, 16.00], sizes:['1/72', '1/48', '1/32'], description:'Pastel com recheio de Chocolate'},
+    {id:4, name:'Pastel de Frango', img:'img/FRANGO.jpg', price:[7.00, 11.00, 15.00], sizes:['1/72', '1/48', '1/32'], description:'Pastel com recheio de Frango'},
+    {id:5, name:'Pastel Mistão', img:'img/MISTAO.jpeg', price:[9.00, 12.00, 16.00], sizes:['1/72', '1/48', '1/32'], description:'Pastel com recheio de Carne, Frango, Queijo, Presunto, Milho e Azeitona'},
+    {id:6, name:'Pastel de Carne', img:'img/pastel-de-carne1.jpg', price:[8.00, 11.00, 16.00], sizes:['1/72', '1/48', '1/32'], description:'Pastel com recheio de Carne'},
+    {id:7, name:'Pastel de Queijo', img:'img/pastel-queijo.jpg', price:[7.00, 11.00, 15.00], sizes:['1/72', '1/48', '1/32'], description:'Pastel com recheio de Queijo'},
+    {id:8, name:'Pastel de Pizza', img:'img/pastel-pizza.jpg', price:[8.00, 11.00, 15.00], sizes:['1/72', '1/48', '1/32'], description:'Pastel com recheio de Queijo, Presunto, Orégano, Tomate e Azeitona'}
+];
